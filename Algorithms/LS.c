269efb0cefb6cc81fd5696cb89c13b984f49fd53
akv2011 <arunkumarv1530@gmail.com>
2024-02-01 01:59:09 +0530

    1:59

--- Algorithms/LS.c
+++ Algorithms/LS.c
@@ -4,29 +4,26 @@
 int main(void)
 {
     int numbers[]={44,55,54,5,33,545,4};
-    int n=get_int("Nuber:\n");
+    int n=get_int("Number:\n");
     for (int i = 0; i <7; i++)
     {
         if(n==numbers[i])
         {
             printf("Found\n");
-            
+            return 0;
+             
         }
-        else
-        {
-            printf("Not a eligible no\n");
-            
-        }
-        
-
-        
+    
     }
+    printf("Not a eligible no\n");
+    return 1;
+}
 
     /*
     if(n==numbers[i])
         {
             printf("Found\n");
-            
+            return 0; on using this is correct number found it exist loop            
         }
         else
         {
@@ -34,9 +31,33 @@ int main(void)
             
         }
     
+    Nuber:
+454
+Not a eligible no
+Not a eligible no
+Not a eligible no
+Not a eligible no
+Not a eligible no
+Not a eligible no
+Not a eligible no
     
+{
+        if(n==numbers[i])
+        {
+            printf("Found\n");
+             
+        }
     
+    }
+    printf("Not a eligible no\n");
+
+
+    Number:
+    4
+    Found
+    Not a eligible no   
+
+
     
     */
     
-}
--- Algorithms/LS1.c
+++ Algorithms/LS1.c
@@ -3,30 +3,7 @@
 
 int main(void)
 {
-    int numbers[]={44,55,54,5,33,545,4};
-    int n=get_int("Number:\n");
-    for (int i = 0; i <7; i++)
-    {
-        numbers[i]=n;
-        printf("Found\n");
-        return 0;
-    }
-    printf("Not Found\n");
-
-    /*
-    printf("Found\n");
-    Without return 0 loop still runs
-    Nuber:
-4
-Found
-Found
-Found
-Found
-Found
-Found
-Found
-
- */
+    
         
      
 }
